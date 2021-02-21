c31fcba4e607a04107bd85c41d26f9bdeb4a1eac
Nawdist <78550645+Nawdist@users.noreply.github.com>
2021-02-21 23:06:09 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,26 @@
+var buttons = document.querySelectorAll(".button");
+var result = document.getElementById("result");
+buttons.forEach(function(item){
+    item.addEventListener("click", function(){
+        result.value += item.innerHTML;
+    })
+})
+var finalResult = document.getElementById("equal").onclick = function(){
+    if(result.value == ""){
+        result.className = "wrong";
+        result.value = "";
+        setTimeout(function(){
+            result.className ="input";
+        }, 3000)
+        
+    } else {
+        result.value = eval(result.value);
+    }
+}
+var back = document.getElementById('back');
+back.addEventListener('click', function(){
+    result.value = result.value.slice(0, -1);
+})
+var clear = document.getElementById("clear").addEventListener('click', function(){
+    result.value = "";
+})
